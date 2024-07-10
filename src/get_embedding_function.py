1759958f9656a4d9bdf011ce35cb55e43f1681ff
Leutrim Uka <leutrim.uka6@gmail.com>
2024-07-10 15:42:45 +0200

    Implement RAG

--- src/get_embedding_function.py
+++ src/get_embedding_function.py
@@ -1,9 +1,28 @@
-from langchain.embeddings.ollama import OllamaEmbeddings
-from langchain.embeddings.bedrock import BedrockEmbeddings
+"""
+This module can be used to experiment with different embedding models. The
+current setup requires Ollama to be installed and running, so that the app
+can establish a connection.
+
+The main function in this module is `get_embedding_function`, which returns
+an instance of the OllamaEmbeddings model.
+
+Note: If you have access to OpenAI or AWS APIs for generating embeddings,
+they will most likely work better than the local version
+"""
+
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+
+# from langchain_huggingface import HuggingFaceEmbeddings
 
 
 def get_embedding_function():
-    embeddings = BedrockEmbeddings(
-        credentials_profile_name="default", region_name="us-east-1"
-    )
+    """
+    Initializes and returns the selected embedding model. Designed to be used
+    with the embed_query method.
+
+    Returns:
+        Initialized embedding model for embedding generation and querying
+    """
+    # embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
     return embeddings
